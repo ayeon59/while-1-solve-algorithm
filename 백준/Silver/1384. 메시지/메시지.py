28bf6437957aed9419dfad59546bb55acb3960c9
A Yeon Jang <125449701+ayeon59@users.noreply.github.com>
2025-05-06 13:36:13 +0900

    [Silver V] Title: 메시지, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1384. 메시지/메시지.py
@@ -0,0 +1,25 @@
+Group = 1
+while True:
+    num = int(input())
+    cnt = 0
+    people=[]
+    if not num : break
+    for _ in range(num): #한사람 한사람 확인
+        people.append(input().split())
+    print(f"Group {Group}")
+    for j in range(num): 
+        for i in range(1,num):
+            if people[j][i]=='P':
+                continue
+            else:
+                cnt = 1
+                p1 = people[(j-i)%num][0]
+                p2 = people[j][0]
+                print(f"{p1} was nasty about {p2}")
+    if(cnt==0):
+        print('Nobody was nasty')
+    Group += 1
+    print()
+
+
+
