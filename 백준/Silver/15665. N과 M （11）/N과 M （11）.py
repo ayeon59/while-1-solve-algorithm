ddb238d061df3d0ca8ad26e87373cd47d7c2041b
A Yeon Jang <125449701+ayeon59@users.noreply.github.com>
2025-06-20 11:22:40 +0900

    [Silver II] Title: N과 M (11), Time: 908 ms, Memory: 70508 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.py
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -1,23 +1,19 @@
 n, m =map(int,input().split())
-set_list=set()
-result = []
+
+
 number = list(map(int,input().split()))
-number.sort()
+set_list=set(number)
+number_new = sorted(list(set_list))
+
 ans = []
 def dfs(step,lst):
     if step == m:
         ans.append(lst)
         return
-    
-    for j in range(n):
-        dfs(step+1,lst+[number[j]])
-        
+    for i in range(len(number_new)):
+        dfs(step+1,lst+[number_new[i]])        
 dfs(0,[])
 
-for element in ans:
-    t = tuple(element)
-    if t not in set_list:
-        set_list.add(t)
-        result.append(element)
-for x in result:
+for x in ans:
     print(*x)
+
