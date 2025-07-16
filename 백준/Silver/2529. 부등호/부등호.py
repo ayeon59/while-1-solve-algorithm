231ae0eb1bac41bf07d390d76d074b10651168ef
A Yeon Jang <125449701+ayeon59@users.noreply.github.com>
2025-07-16 16:06:41 +0900

    [Silver I] Title: 부등호, Time: 2024 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.py
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -2,8 +2,28 @@ from itertools import permutations
 
 n = int(input())
 comparison = list(input().split())
-answer = []
 digits = [i for i in range(10)]
+
+
+for perm in permutations (reversed(digits),n+1):
+    
+    perm = list(perm)
+    isCorrect = True
+    for i in range(n):
+        if isCorrect :
+            if comparison[i] == "<":
+                if perm[i] < perm[i+1] :
+                    continue
+                else :
+                    isCorrect = False
+            else :
+                if perm[i] > perm[i+1] :
+                    continue
+                else :
+                    isCorrect = False
+    if(isCorrect) : 
+        print(''.join(map(str,perm)))
+        break
 
 for perm in permutations (digits,n+1):
     
@@ -22,9 +42,9 @@ for perm in permutations (digits,n+1):
                 else :
                     isCorrect = False
     if(isCorrect) : 
-        answer.append(perm)
+        print(''.join(map(str,perm)))
+        break
 
-print(''.join(map(str, answer[-1])))
-print(''.join(map(str, answer[0])))
+
 
 
