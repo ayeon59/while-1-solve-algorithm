af4a8e91433b9211b99accf305a67dd0d64cbf8d
A Yeon Jang <125449701+ayeon59@users.noreply.github.com>
2025-07-16 17:55:11 +0900

    [Silver V] Title: 영화감독 숌, Time: 656 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,9 +1,11 @@
 n = int(input())
-count_666 = 0
-num = str(665)
-while count_666 != n :
-    num = str(int(num)+1)
-    if num.count("666") >= 1 :
-        count_666 += 1
+count = 0
+current = 666
 
-print(num)
+while True:
+    if "666" in str(current):
+        count += 1
+        if count == n:
+            print(current)
+            break
+    current += 1
