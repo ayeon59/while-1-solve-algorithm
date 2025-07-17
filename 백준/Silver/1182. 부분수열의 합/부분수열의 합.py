18f5b056d1b39304ae1832bc3c881f831f8a648d
A Yeon Jang <125449701+ayeon59@users.noreply.github.com>
2025-07-17 11:17:36 +0900

    [Silver II] Title: 부분수열의 합, Time: 212 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,17 +1,20 @@
 n, s = map(int,input().split())
-number = list(map(int,input().split()))
+a = list(map(int,input().split()))
 count = 0
 
-def dfs(step,sum):
+def dfs(depth,sum):
     global count
-    if step == n :
-        if sum == s:
-            count+= 1
-        return 
-    dfs(step+1,sum + number[step])
-    dfs(step+1,sum)
-
+    if depth == n :
+        if sum == s :
+            count += 1
+        return
+    
+    dfs(depth+1,sum + a[depth])
+    dfs(depth+1,sum)
+            
 dfs(0,0)
+
 if s == 0:
-    count -= 1
+    count -=1
+
 print(count)
