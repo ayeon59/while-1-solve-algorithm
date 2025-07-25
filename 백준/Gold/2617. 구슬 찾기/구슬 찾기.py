857b1ba29686e3e727b5b9d05a23212c032a0b12
A Yeon Jang <125449701+ayeon59@users.noreply.github.com>
2025-07-26 00:02:24 +0900

    [Gold IV] Title: 구슬 찾기, Time: 364 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.py
@@ -0,0 +1,28 @@
+n, m = map(int,input().split()) 
+graph1 = [[0]*(n+1) for _ in range(n+1)]
+graph2 = [[0]*(n+1) for _ in range(n+1)]
+scope = n//2 + 1
+
+answer = set()
+
+for _ in range(m):
+    a,b = map(int,input().split())
+    graph1[b][a] = 1
+    graph2[a][b] = 1
+
+
+for k in range(n+1):
+    for i in range(n+1):
+        for j in range(n+1):
+            if graph1[i][k] == 1 and graph1[k][j] == 1:
+                graph1[i][j] = 1
+            if graph2[i][k] == 1 and graph2[k][j] == 1:
+                graph2[i][j] = 1
+
+for i in range(1,n+1):
+    if graph1[i].count(1) >= scope:
+        answer.add(i)
+    if graph2[i].count(1) >= scope:
+        answer.add(i)
+
+print(len(answer))
