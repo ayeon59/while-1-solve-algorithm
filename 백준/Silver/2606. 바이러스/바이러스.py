9a382ec66468b71b677694e84a01acbc87b0880e
A Yeon Jang <125449701+ayeon59@users.noreply.github.com>
2025-07-25 11:25:06 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,27 +1,20 @@
-from collections import deque
-n = int(input())
-m = int(input())
-computer = [[] for _ in range(n+1)]
-visited = [False] * (n+1) 
-count = 0
+computer = int(input())
+network = int(input())
+visited = [False] * (computer+1)
+virus = [[] for _ in range(computer+1)]
 
-for _ in range(m):
-    a, b = map(int,input().split())
-    computer[a].append(b)
-    computer[b].append(a)
 
-def bfs(start):
-    global count
-    q = deque([start])
-    visited[start] = True
-    while q :
-        element = q.popleft()
+def dfs(v_computer):
+    visited[v_computer] = True
+    for next_computer in virus[v_computer]:
+        if not visited[next_computer]:            
+            dfs(next_computer)
 
-        for next in computer[element]:
-            if not visited[next] :
-                count += 1
-                visited[next] = True
-                q.append(next)
+for i in range(network):
+    a,b = map(int,input().split())
+    virus[a].append(b)
+    virus[b].append(a)
 
-bfs(1)
-print(count)
+
+dfs(1)
+print(visited.count(True)-1)
