9d99b74a8315e02251ad2b7ae7f1eff49cc46eb2
A Yeon Jang <125449701+ayeon59@users.noreply.github.com>
2025-07-25 11:04:41 +0900

    [Silver II] Title: DFS와 BFS, Time: 312 ms, Memory: 35268 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,39 +1,41 @@
 from collections import deque
 
-n,m,v=map(int,input().split())
-algorithm = [[False] * (n+1) for _ in range(n+1)]
+n, m, v = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
 
-visited = [False] * (n+1)
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-for i in range(m):
-    a,b = map(int,input().split())
-    algorithm[a][b] = True
-    algorithm[b][a] = True
-
-def dfs(idx):
-    global visited
-    visited[idx] = True
-    print(idx, end=" ")
-    for next in range(n+1) :
-        if not visited[next] and algorithm[idx][next] == True:
-            dfs(next)
-
-def bfs():
-    global q, visited
-    while q :
-        cur = q.popleft()
-        print(cur, end=" ")
-        for next in range(n+1):
-            if not visited[next] and algorithm[cur][next] == True:
-                visited[next] = True
-                q.append(next)
+for i in range(1, n + 1):
+    graph[i].sort()
 
 
-dfs(v)
-print() 
-visited = [False] * (n+1)
-q = deque([v])
-visited[v] = True
-bfs()
+def dfs(node, visited):
+    visited[node] = True
+    print(node, end=' ')
+    for neighbor in graph[node]:
+        if not visited[neighbor]:
+            dfs(neighbor, visited)
 
 
+def bfs(start):
+    queue = deque([start])
+    visited[start] = True
+    print(start, end=' ')
+    while queue:
+        node = queue.popleft()
+        for neighbor in graph[node]:
+            if not visited[neighbor]:
+                visited[neighbor] = True
+                print(neighbor, end=' ')
+                queue.append(neighbor)
+
+
+visited = [False] * (n + 1)
+dfs(v, visited)
+print()
+
+visited = [False] * (n + 1)
+bfs(v)
