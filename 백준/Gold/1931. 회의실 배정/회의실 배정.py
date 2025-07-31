c04339f7c8859bd685ac4c57a7d641970f939256
A Yeon Jang <125449701+ayeon59@users.noreply.github.com>
2025-07-31 23:37:24 +0900

    [Gold V] Title: 회의실 배정, Time: 2836 ms, Memory: 55240 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,18 +1,16 @@
+
 n = int(input())
 meeting = []
-
-for i in range(n):
-    start,end = map(int,input().split())
-    meeting.append((start,end))
-
-count = 0
-sorted_meeting = sorted(meeting, key=lambda x: (x[1], x[0]))
-
+for _ in range(n):
+    a, b = map(int,input().split())
+    meeting.append([a,b])
+meeting.sort(key=lambda x: (x[1], x[0]))
+cnt = 0
 end_time = 0
+for start,end in meeting:
+    if start >= end_time :
+        cnt += 1
+        end_time = end 
 
-for start, end in sorted_meeting:
-    if start >= end_time:
-        count += 1
-        end_time = end
-
-print(count)
+print(cnt)
+    
